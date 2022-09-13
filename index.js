f4f1a85165464ce243695133fc32a4e5b52347d2
Megan Rowan <megan.rowan.88@gmail.com>
2022-09-13 00:18:49 -0400

    commit tree 5dd353472cb67453a67a9178dcc3fc96b301a96b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+   return cats.push("Ralph");
+};
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+};
+
+const allCats = [...cats, "Broom"];
+
+function appendCat(name) {
+    return allCats;
+};
+
+const newCats = ["Arnold", ...cats];
+
+function prependCat(name) {
+    return newCats;
+};
+
+function removeLastCat(name) {
+    return cats.slice(0, -1);
+};
+
+function removeFirstCat(name) {
+    return cats.slice(1, cats.length)
+}
